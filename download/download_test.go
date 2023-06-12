5679907be341bf2e7cf8aeff5eb02fd851115639
Hal Canary <halcanary@gmail.com>
2023-06-12 12:24:46 -0400

    finish update

--- download/download_test.go
+++
@@ -1,8 +0,0 @@
-package download
-
-// Copyright 2022 Hal Canary
-// Use of this program is governed by the file LICENSE.
-
-import "testing"
-
-func TestDownload(t *testing.T) {}
--- ebook/ebook_test.go
+++
@@ -1,8 +0,0 @@
-package ebook
-
-// Copyright 2022 Hal Canary
-// Use of this program is governed by the file LICENSE.
-
-import "testing"
-
-func TestEbook(t *testing.T) {}
